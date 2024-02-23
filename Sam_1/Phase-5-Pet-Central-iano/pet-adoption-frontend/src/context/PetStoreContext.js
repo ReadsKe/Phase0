a3090f772a6fb2cd04f3d40f32c4ea4f2c896237
Samuel Kimote <readssamuel@.com>
2024-02-23 10:18:26 +0300

    update

--- Sam_1/Phase-5-Pet-Central-iano/pet-adoption-frontend/src/context/PetStoreContext.js
+++ Sam_1/Phase-5-Pet-Central-iano/pet-adoption-frontend/src/context/PetStoreContext.js
@@ -4,38 +4,41 @@ export const PetStoreContext = createContext();
 
 const PetStoreContextProvider = ({ children }) => {
   const [petStores, setPetStores] = useState([]);
-  const[selectedPetstore, setSelectedPetstore] = useState(null)
-  const [onChange , setOnChange] = useState(false)
+  const [selectedPetstore, setSelectedPetstore] = useState(null);
+  const [onChange, setOnChange] = useState(false);
 
-  //fetch pet stores
-  useEffect(()=>{
+  // Fetch pet stores
+  useEffect(() => {
     console.log("Fetching pet stores...");
-     fetch('/petstores')
-    .then(res => res.json())
-    .then(response =>{
-      setPetStores(response)
+    fetch('/petstores')
+      .then(res => res.json())
+      .then(response => {
+        setPetStores(response);
       })
-    },[onChange])
+      .catch(error => console.error('Error fetching pet stores:', error));
+  }, [onChange]);
 
-    //fetch pet store by ID
-    const fetchPetStoreById = (petstoreId) =>{
-      fetch(`/petstores/${petstoreId}`)
+  // Fetch pet store by ID
+  const fetchPetStoreById = (petstoreId) => {
+    fetch(`/petstores/${petstoreId}`)
       .then((res) => res.json())
       .then((response) => {
         setSelectedPetstore(response);
-      });
-    }
+      })
+      .catch(error => console.error('Error fetching pet store by ID:', error));
+  }
 
-    //context data
-    const contextData = {
-      petStores,
-      selectedPetstore,
-      fetchPetStoreById,
-      onChange,
-      setOnChange,
-    }
+  // Context data
+  const contextData = {
+    petStores,
+    selectedPetstore,
+    fetchPetStoreById,
+    onChange,
+    setOnChange,
+  };
+
   return (
-    <PetStoreContext.Provider value={{ petStores, fetchPetStoreById }}>
+    <PetStoreContext.Provider value={contextData}>
       {children}
     </PetStoreContext.Provider>
   );
